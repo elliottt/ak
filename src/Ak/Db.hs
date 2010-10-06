5e988324225848063a11d3699fa75e5fffd68e3f
Jonathan Daugherty <jtd@galois.com>
2010-10-06 15:46:52 -0700

    Use Ak.Common.taskFilename in Ak.Db

--- src/Ak/Db.hs
+++ src/Ak/Db.hs
@@ -14,10 +14,13 @@ import System.Directory
     ( getCurrentDirectory, doesFileExist )
 import System.FilePath
     ( takeDirectory, (</>) )
+import Ak.Common
+    ( taskFilename )
 
--- | The DbSpec describes all parent paths where a .ak file exists.  There is no
--- guarantee that this is the full set of .ak files that will be used when
--- re-assembling the database, as any of them could be marked as roots.
+-- | The DbSpec describes all parent paths where a .ak file exists.
+-- There is no guarantee that this is the full set of .ak files that
+-- will be used when re-assembling the database, as any of them could
+-- be marked as roots.
 type DbSpec = [FilePath]
 
 -- | Generate a list of the parents of a filename.
@@ -30,7 +33,7 @@ directoryParents path
 
 -- | Discover a @DbSpec@ from any directory
 discoverDbSpecFrom :: FilePath -> IO DbSpec
-discoverDbSpecFrom  = filterM doesFileExist . map (</> ".ak") . directoryParents
+discoverDbSpecFrom  = filterM doesFileExist . map (</> taskFilename) . directoryParents
 
 -- | Discover a @DbSpec@ from the current directory.
 discoverDbSpec :: IO DbSpec
